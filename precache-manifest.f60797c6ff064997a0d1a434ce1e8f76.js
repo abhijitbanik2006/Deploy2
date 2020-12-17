042f8099d370fc3079aee04d13dba3513355823f
Abhijit Banik <51152286+abhijitbanik2006@users.noreply.github.com>
2020-12-17 16:39:02 +0530

    Add files via upload

---
+++ precache-manifest.f60797c6ff064997a0d1a434ce1e8f76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4346025a40c423c19ee5ca12160682cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69bbc70bc89975b50887",
+    "url": "/static/css/main.fc7e2061.chunk.css"
+  },
+  {
+    "revision": "7f923c9b7b5dd4c8d95f",
+    "url": "/static/js/2.75c18bb4.chunk.js"
+  },
+  {
+    "revision": "53b3eb61d55f97b94db184462bf2e44d",
+    "url": "/static/js/2.75c18bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b8226a9527aeb9f357",
+    "url": "/static/js/3.6901dbd0.chunk.js"
+  },
+  {
+    "revision": "69bbc70bc89975b50887",
+    "url": "/static/js/main.53d23f5f.chunk.js"
+  },
+  {
+    "revision": "4c47b67ebe32b99ff552",
+    "url": "/static/js/runtime-main.c4502ff5.js"
+  }
+]);
